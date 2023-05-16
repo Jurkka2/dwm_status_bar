eef322f7e1a097f95ae575e55f787b738e5514e0
Jurkka2 <jurkka.breindl@posteo.de>
2023-05-16 09:31:56 +0200

    some stuff

--- main_dwm.sh
+++ main_dwm.sh
@@ -1,5 +1,8 @@
 
 
-xsetroot -name """$(date '+%Y-%m-%d %H:%M'  && printf ' | ' &&  printf 'Battery:' && upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {gsub(/%/, ""); print $2 }'  )"""
+date=`date '+%Y-%m-%d %H:%M'`
+bat=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {gsub(/%/, ""); print $2 }' `
 
+
+xsetroot -name """$( printf " | " ; printf "Bat: $bat" ; printf " | " ; printf "Time: $date" ; printf " |" ; )"""
 
