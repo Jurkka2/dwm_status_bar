a5a86a763b30335e0b673e2279800951c53dc044
Jurkka2 <jurkka.breindl@posteo.de>
2023-05-16 11:47:34 +0200

    stuff

--- main_dwm.sh
+++ main_dwm.sh
@@ -4,6 +4,8 @@ while [ true ]
 do
 	date=`date '+%Y-%m-%d %H:%M:%S'`
 	bat=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {gsub(/%/, ""); print $2 }' `
+	ton=`amixer get Master | awk -F '[][]' '/%/ { print $2 }' | awk '{ print $1 }'`
+	load=`cat /proc/loadavg | awk '{print $3}'`
 
-	xsetroot -name """$( printf " | " ; printf "Bat: $bat" ; printf " | " ; printf "Time: $date" ; printf " |" ; )"""
+	xsetroot -name """$( printf " | "; printf "L15m: $load" ; printf " | ";  printf "Ton: $ton"; printf " | " ; printf "Bat: $bat" ; printf " | " ; printf "Time: $date" ; printf " |" ; )"""
 done 
