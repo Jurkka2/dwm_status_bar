f47ed18881180abcf0b1827f77e3851bfd7c2af7
Jurkka2 <jurkka.breindl@posteo.de>
2023-05-16 11:25:45 +0200

    stuff

--- main_dwm.sh
+++ main_dwm.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+set +x
 
+while [ true ] 
+do
+	date=`date '+%Y-%m-%d %H:%M:%S'`
+	bat=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {gsub(/%/, ""); print $2 }' `
 
-date=`date '+%Y-%m-%d %H:%M'`
-bat=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {gsub(/%/, ""); print $2 }' `
-
-
-xsetroot -name """$( printf " | " ; printf "Bat: $bat" ; printf " | " ; printf "Time: $date" ; printf " |" ; )"""
-
+	xsetroot -name """$( printf " | " ; printf "Bat: $bat" ; printf " | " ; printf "Time: $date" ; printf " |" ; )"""
+done 
